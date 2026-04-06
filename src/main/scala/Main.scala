d81b48f417442070ed154d82b5e0aafcfc1959f7
Rafael Ceratto <rafaelceratto2004@mi.unc.edu.ar>
2026-04-06 20:37:35 -0300

    Ejercicio 6 funcionando

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -32,14 +32,38 @@ object Main {
       s"--- Posts extraídos de: $url ---\n" + postsFormateados       //Retornamos el bloque de texto para esta URL
     }.mkString("\n\n") //Separamos los bloques de diferentes URLs con un salto de línea
     println(outputParseado)    //Imprimimos el resultado final
+
+    //Agregado para ejercicio 6
+    subscriptions.zip(allPosts).foreach { case (sub, (url, postsCrudos)) =>  //recorremos las subs con sus posts descargados
+      val nombreSub = sub._1 //ahora tengo el nombre de la subscripcion.
+      val listaDePosts = FileIO.parsePost(postsCrudos) //transformo el texto de JSON, a algo mas trabajable
+      val postsFiltrados = TextProcessing.filterPosts(listaDePosts) //Filtro los vacios o invalidos
+
+      val scoreTotal = TextAnalytics.calculateTotalScore(postsFiltrados) //obtengo el score
+      val frecuencias = TextAnalytics.getWordsFrequencies(postsFiltrados) //obtengo las palabras
+
+      println(s"REPORTE DE $nombreSub")
+      println(s"\nScore: $scoreTotal")
+
+      println("\nPalabras mas frecuentes:")
+      frecuencias.take(5).foreach { case (palabra, cantidad) =>  //imprimo las palabras
+        println(s" * $palabra: $cantidad") 
+      }
+
+      println("\nPrimeros 5 posts:")
+      postsFiltrados.take(5).zipWithIndex.foreach { case (post, i) =>
+        val (subreddit, title, selftext, date, score) = post
+        println(s"${i + 1}. $title | Fecha: $date | URL: $url")
+      }
   
+      println("-" * 30) //imprimo lineas para que quede mas estetico
+    }
    //Agregado para punto estrella
     println("\n" + "=" * 40)
     val respuesta = scala.io.StdIn.readLine("¿Desea ingresar al Menú Interactivo de Lectura? (S/N): ")
 
     if (respuesta.toLowerCase == "s") { //si se desea entrar adapto los datos de entrada y llamo a mi funcion.
-      val subsParaMenu = subscriptions.map(_.asInstanceOf[(String, String)]) //aca agarro toda mi lista de suibscripciones y 
-                                                                             //las envio a una nueva lista con formato (Nombre, URL)
+      val subsParaMenu = subscriptions.map(_.asInstanceOf[(String, String)]) //aca agarro toda mi lista de suibscripciones y las envio a una nueva lista con formato (Nombre, URL)
       val postsParaMenu = allPosts.map(tupla => tupla._2) //aca ignoro la primer tupla que son los url, y agarro el texto en JSON
       
       UserInterface.mostrarMenu(subsParaMenu, postsParaMenu) //llamo a mi funcion con las variables ya corregidas para que ande el programa.
